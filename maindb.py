00d11d5b80ba9efe3ca9bfe9bf3a10474bf9f99e
Owen Stelmarski <ostelmarski@gmail.com>
2019-02-17 02:14:02 -0600

    MAIN DB REWRITE

--- maindb.py
+++ maindb.py
@@ -1,4 +1,6 @@
-import requests, json, time, datetime, random
+import requests, json, time, datetime, random, smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from firebase import firebase
 
 global FirebaseURL
@@ -20,10 +22,11 @@ def GetData():
 
 #Get All Data From Specific User
 def IterateData(UserName, Firebase):
-    CurentUser = Firebase.get('/Users', f'{str(UserName)}')
-    UserInfo = CurentUser['UserInfo']
-    UserParking = CurentUser['UserParking']
+    CurrentUser = Firebase.get('/Users', f'{str(UserName)}')
+    UserInfo = CurrentUser['UserInfo']
+    UserParking = CurrentUser['UserParking']
 
+    #UserEmail = UserInfo['email']
     UserFullName = UserInfo['name']
     #UserPassword = UserInfo['password']
     UserPhone = UserInfo['phone']
@@ -43,30 +46,107 @@ def IterateData(UserName, Firebase):
     return UserFullName, UserPhone, arr
 
 #Add User To Database
-def RegisterUser():
-    UserName = input("UserName: ")
-    firebase = firebase.FirebaseApplication(FirebaseURL, None)
-    UserData = {
-        "UserInfo": {
-            "name": UserFullName,
-            "password": UserPassword,
-            "phone": UserPhone
-        },
-        "UserParking":  {
-            "address":LotLocation,
-            "count": LotCount,
-            "imageURL": ImageURL,
-            "lotname": LotName,
-            "pricing": LotPPH,
-            "time-end": LotTimeEnd,
-            "time-start": LotTimeStart
-        }
-    }
-    firebase.post(f'/Users/{str(UserName)}', None, UserData)
+def RegisterUser(UserName, UserFullName, UserEmail, UserPassword, UserPhone):
+    Firebase = firebase.FirebaseApplication(FirebaseURL, None)
+    UserExist, Firebase = CheckUser(UserName)
+    if UserExist == True:
+        print("Username Unavailable")
+        return False
+    else:
+        print("Username Available")
+        UserData = {
+            "UserInfo": {
+                "email": UserEmail,
+                "name": UserFullName,
+                "password": UserPassword,
+                "phone": UserPhone
+            }
+        }  
+        Firebase.put(f'Users', UserName, UserData)
+        return True
 
 #Add A Lot
-def AddLot():
+def AddLot(UserName, LotLocation, LotCount, LotImage, LotName, LotPPH, LotTimeEnd, LotTimeStart):
     LotID = ""
     for x in range(24):
         Letter = random.choice(LotIDString)
         LotID = LotID + Letter
+
+    Firebase = firebase.FirebaseApplication(FirebaseURL, None)
+    LotData = {
+        "address": LotLocation,
+        "count": int(LotCount),
+        "imageURL": LotImage,
+        "lotname": LotName,
+        "pricing": int(LotPPH),
+        "time-end": LotTimeEnd,
+        "time-start": LotTimeStart
+    }
+    Firebase.put(f'Users/{str(UserName)}/UserParking', LotID, LotData)
+
+#Check If User Exists In DB (REGISTER + LOGIN)
+def CheckUser(UserName):
+    Firebase = firebase.FirebaseApplication(FirebaseURL, None)
+    UsersData = Firebase.get('/Users', None)
+
+    for User in UsersData:
+        if UserName.lower() == User.lower():
+            return True, Firebase
+        else:
+            pass
+    return False
+
+#Login User Through Site
+def LoginUser(UserName, PasswordEntered):
+    UserExist, Firebase = CheckUser(UserName)
+    if UserExist:
+        CurrentUser = Firebase.get('/Users', f'{str(UserName)}')
+        UserPassword = CurrentUser['UserInfo']['password']
+
+        if UserPassword == PasswordEntered:
+            return True
+        else:
+            print("Incorrect Username or Password")
+            return False
+
+    else:
+        print("Incorrect Username or Password")
+        return False
+
+#Remove Lot On Sale Or Removal
+def RemoveLot(UserName, LotID):
+    Firebase = firebase.FirebaseApplication(FirebaseURL, None)
+    Firebase.delete(f"Users/{str(UserName)}/UserParking", LotID)
+
+#Send Purchase Email
+def SendEmail(receiver_email, CustomerName):
+    sender_email = "LendASpacePurchase@gmail.com"
+    password = "AdminLogin22"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Your Lot From Lend-A-Space"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    html = f"""\
+    <html>
+    <body>
+        <p>Hi, {str(CustomerName)}<br>
+        How are you?<br>
+        <a href="http://www.realpython.com">Here is information on your Lend-A-Space!</a> 
+        </p>
+    </body>
+    </html>
+    """
+
+    part1 = MIMEText(html, "html")
+    message.attach(part1)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email,
+            receiver_email,
+            message.as_string(),
+        )
